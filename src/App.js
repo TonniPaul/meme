f9982eecefb44a2644e96f28e37d7cb2754ae81b
TonniPaul <ariyoadeoyepaul@gmail.com>
2022-07-20 09:52:06 +0100

    Added Footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./componenets/Header";
 import Form from "./componenets/Form";
+import Footer from "./componenets/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Form/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/componenets/Footer.js
@@ -0,0 +1,8 @@
+function Footer(){
+   return (
+      <footer>
+         TonniPaul Inc. &copy; 2022.
+      </footer>
+   )
+}
+export default Footer;
