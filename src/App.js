07e070cc9942df48baf05161d15af8905182a516
TonniPaul <ariyoadeoyepaul@gmail.com>
2022-07-25 01:53:41 +0100

    Updated input to insert text on Image

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
     <div className="bodyyy">
       <Header/>
       <div className="App">
-        
         <Form/>
       </div>
       <Footer/>
--- src/componenets/Form.js
+++ src/componenets/Form.js
@@ -1,14 +1,32 @@
 import {memeImages}from "../componenets//Images";
-import { useState } from "react";
+import React, { useState } from "react";
 
 function Form (){
-   const [memeImage, setMemeImage] = useState("https://i.imgflip.com/1g8my4.jpg");
+   const [meme, setMeme] = useState({
+         toptext: "",
+         bottomtext: "",
+         randomImage:"https://i.imgflip.com/1g8my4.jpg"
+   }
+   )
+   const [allMemesImages,]= useState(memeImages)
 
    function getMemeImage (){
-      const memesArray = memeImages.data.memes;
+      const memesArray = allMemesImages.data.memes;
       const randomNumber = Math.floor(Math.random() * memesArray.length);
-      setMemeImage(memesArray[randomNumber].url);
+      const url = memesArray[randomNumber].url
+      setMeme(prevMeme => ({
+         ...prevMeme,
+         randomImage: url
+      }));
    }  
+
+   function handleChange(event) {
+      const {name, value} = event.target
+      setMeme(prevMeme => ({
+          ...prevMeme, 
+          [name]: value
+      }))
+  }
    return(
       <div className="bodycont">
          <div className="form">
@@ -16,11 +34,17 @@ function Form (){
                type="text" 
                placeholder="Your Header here"
                id="head--text"
+               name="toptext"
+               value={meme.toptext}
+               onChange={handleChange}
             />
             <input 
                type="text" 
                placeholder="Your footer here"
                id="foot--text"
+               name="bottomtext"
+               value={meme.bottomtext}
+               onChange={handleChange}
             />
             <input 
                type="button" 
@@ -29,7 +53,12 @@ function Form (){
                onClick={getMemeImage}
             />
          </div>
-         <img src={memeImage} className="image" alt="mm"/>
+         <div className="meme">
+            <img src={meme.randomImage} className="image" alt="mm"/>
+             <h2 className="meme--text top">{meme.toptext}</h2>
+            <h2 className="meme--text bottom">{meme.bottomtext} </h2>
+         </div>
+         <div className="empty"></div>
       </div>
    );
 }
